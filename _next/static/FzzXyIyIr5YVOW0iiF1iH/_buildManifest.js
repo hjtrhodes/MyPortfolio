82553e2fa008f5038f3cc5d5a68b84a655ee9ced
hjtrhodes <hjtrhodes@gmail.com>
2024-01-16 13:06:18 +0000

    Deploying to gh-pages from @ hjtrhodes/MyPortfolio@17b41b186d3effcfa5e588969910b7c243a19dcf 🚀

---
+++ _next/static/FzzXyIyIr5YVOW0iiF1iH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/809-90af088cb39c18d2118b.js","static/chunks/pages/index-155f2ef321c0c2096d10.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
