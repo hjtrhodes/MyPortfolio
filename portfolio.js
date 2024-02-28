3f4804dd34ece299ea98ba75d334bd5f40f802a7
Harry Rhodes <hjtrhodes@gmail.com>
2024-02-28 19:03:52 +0000

    Update to sympols in Greeting section

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,12 @@
 import emoji from "react-easy-emoji";
 
-
 export const greetings = {
   name: "Harry Rhodes",
   title: "Welcome to my Portfolio.",
-  intro: "Hi, I'm Harry. I'm a Software Developer and graduate from Makers' Full Stack Software Development course. I love problem-solving, learning and understanding how things work!",
-  CVLink: "https://docs.google.com/document/d/1WDQ-0gyIWZ4Quz8XPf9fVCJSYeT4G6iH9QUZh5Qx7nI/edit?usp=sharing",
+  intro:
+    "Hi, I'm Harry. I'm a Software Developer and graduate from Makers' Full Stack Software Development course. I love problem-solving, learning and understanding how things work!",
+  CVLink:
+    "https://docs.google.com/document/d/1WDQ-0gyIWZ4Quz8XPf9fVCJSYeT4G6iH9QUZh5Qx7nI/edit?usp=sharing",
 };
 
 export const greetingsSkills = {
@@ -15,7 +16,7 @@ export const greetingsSkills = {
         {
           skillName: "JavaScript",
           fontAwesomeClassname: "logos:javascript",
-        },        
+        },
         {
           skillName: "React",
           fontAwesomeClassname: "logos:react",
@@ -23,21 +24,26 @@ export const greetingsSkills = {
         {
           skillName: "Express",
           fontAwesomeClassname: "devicon:express",
-        },  
+        },
         {
           skillName: "MongoDB",
           fontAwesomeClassname: "logos:mongodb-icon",
         },
+        // {
+        //   skillName: "Nodejs",
+        //   fontAwesomeClassname: "logos:nodejs-icon",
+        // },
+        // {
+        //   skillName: "Jest",
+        //   fontAwesomeClassname: "logos:jest",
+        // }
+      ],
+
+      softwareSkills2: [
         {
           skillName: "Nodejs",
           fontAwesomeClassname: "logos:nodejs-icon",
         },
-        {
-          skillName: "Jest",
-          fontAwesomeClassname: "logos:jest",
-        }],
-        
-        softwareSkills2: [
         {
           skillName: "Python",
           fontAwesomeClassname: "logos:python",
@@ -50,14 +56,14 @@ export const greetingsSkills = {
           skillName: "PostgreSQL",
           fontAwesomeClassname: "logos:postgresql",
         },
-        {
-          skillName: "Pytest",
-          fontAwesomeClassname: "devicon:pytest",
-        },
-        {
-          skillName: "Cypress",
-          fontAwesomeClassname: "logos:cypress-icon",
-        },
+        // {
+        //   skillName: "Pytest",
+        //   fontAwesomeClassname: "devicon:pytest",
+        // },
+        // {
+        //   skillName: "Cypress",
+        //   fontAwesomeClassname: "logos:cypress-icon",
+        // },
       ],
     },
   ],
@@ -77,27 +83,18 @@ export const socialLinks = {
 
 export const skillsSection = {
   title: "Technologies and Skills",
-  subTitle: "Below are some of my skills and the technologies I have experience with so far, but I code everyday and am constantly upskilling. Currently I'm deepening my knowledge of effective React development and working with Redux.",
+  subTitle:
+    "Below are some of my skills and the technologies I have experience with so far, but I code everyday and am constantly upskilling. Currently I'm deepening my knowledge of effective React development and working with Redux.",
   data: [
     {
       title: "",
       lottieAnimationFile: "/lottie/webdev.json", // Path of Lottie Animation JSON File
       skills: [
-        emoji(
-          "⚡ Adept problem-solver."
-        ),
-        emoji(
-          "⚡ Excellent communicator.",
-        ),
-        emoji(
-          "⚡ Collaborative team worker.",
-        ),
-        emoji(
-          "⚡ Highly resourceful.",
-        ),
-        emoji(
-          "⚡ Consistently proactive.",
-        ),
+        emoji("⚡ Adept problem-solver."),
+        emoji("⚡ Excellent communicator."),
+        emoji("⚡ Collaborative team worker."),
+        emoji("⚡ Highly resourceful."),
+        emoji("⚡ Consistently proactive."),
       ],
       softwareSkills: [
         {
@@ -190,7 +187,8 @@ export const educationInfo = [
     descBullets: [
       "As an aspiring tech professional with a background as a primary teacher and year group leader, I have a proven track record of effective collaboration and communication when leading teams and maintaining positive relationships with a plethora of stakeholders; strong adaptation and problem solving skills when helping individuals to make progress, regardless of their needs; and a budding passion for all things technological after utilising technology to inspire young people.",
       "After taking time to expose myself to basic programming in HTML, CSS and Python I realised how much I enjoyed the challenge of solving coding problems and mastering new ideas. As a result I made the exciting decision to pursue a career in Tech and completed the Makers Software Development bootcamp. It was there I was pleased to discover that abilities honed in my previous career, such as simplifying and breaking down concepts, collaborating with a team to pursue a shared goal and my desire to learn, have served me well when mastering the new technologies, programming languages, and development tools I have been exposed to.",
-      "In my new career I am looking for an opportunity to add value to a collaborative and innovative environment. I am excited to progress my skills by deepening my current knowledge base and challenging myself to learn even more new technologies."    ],
+      "In my new career I am looking for an opportunity to add value to a collaborative and innovative environment. I am excited to progress my skills by deepening my current knowledge base and challenging myself to learn even more new technologies.",
+    ],
     github: "https://github.com/hjtrhodes",
   },
   {
@@ -200,15 +198,15 @@ export const educationInfo = [
     grade: "",
     desc: "",
     descBullets: [
-      "I am a father to my adorable one year old son, Freddie, and husband to Madeleine. I moved to a small town just outside Reading last year but previously lived in Central London for ten years.", 
+      "I am a father to my adorable one year old son, Freddie, and husband to Madeleine. I moved to a small town just outside Reading last year but previously lived in Central London for ten years.",
       "Originally, I am from Jersey in the Channel Islands. I lived there for 21 years before I moved to London. I still have family and friends there who I try to visit as much as possible.",
       "I went to the University of Exeter for my Bachelors Degree and UEL for my Masters. My wife and I met during our time at Exeter University and love going back to visit the city.",
       "I love sports: running, cycling, Formula 1, rugby, weightlifting and American football all find a place on my list of interests. I am a cyclist myself and love exploring the countryside on my bike.",
-      "I have always enjoyed unraveling and understanding the workings of things, be it hardware or software. Whether it's DIY around the house or fixing motorbikes during my teenage years; I have always been a tinkerer, taking things apart, working out the problems and fixing them.", 
-      "This pleasure now extends to coding. I enjoy consistently expanding my knowledge to meet the challenge infront of me, deepening my understanding of the logic and syntax along the way. I am incredibly excited to continue my professional journey and see how much more I can learn!"
+      "I have always enjoyed unraveling and understanding the workings of things, be it hardware or software. Whether it's DIY around the house or fixing motorbikes during my teenage years; I have always been a tinkerer, taking things apart, working out the problems and fixing them.",
+      "This pleasure now extends to coding. I enjoy consistently expanding my knowledge to meet the challenge infront of me, deepening my understanding of the logic and syntax along the way. I am incredibly excited to continue my professional journey and see how much more I can learn!",
     ],
     github: "https://github.com/hjtrhodes",
-  }
+  },
 ];
 
 export const experience = [
@@ -217,32 +215,29 @@ export const experience = [
     company: "Makers Academy",
     companylogo: "/img/icons/common/Makers.jpeg",
     date: "September 2023 – January 2024",
-    desc:
-      "Software Development Graduate",
+    desc: "Software Development Graduate",
   },
   {
     role: "",
     company: "University of East London",
     companylogo: "/img/icons/common/ovs.png",
     date: "September 2016 – August 2018",
-    desc:
-    "Master of Arts in Education, Pass with Distinction",
+    desc: "Master of Arts in Education, Pass with Distinction",
   },
   {
     role: "",
     company: "University of East London",
     companylogo: "/img/icons/common/ovs.png",
     date: "September 2014 – August 2015",
-    desc:
-    "Postgraduate Certificate of Education",
+    desc: "Postgraduate Certificate of Education",
   },
   {
     role: "",
     company: "University of Exeter",
     companylogo: "/img/icons/common/chamber.jpeg",
     date: "September 2008 – August 2011",
-    desc:
-      "Bachelor of Arts with Honours in Sociology, 2:1"  },
+    desc: "Bachelor of Arts with Honours in Sociology, 2:1",
+  },
 ];
 
 export const projects = [
@@ -250,13 +245,13 @@ export const projects = [
     name: "Bald Bible",
     img: "/img/icons/common/BaldBible.jpeg",
     mov: "RZVKPn00WF8",
-    desc:
-      "Pinterest style, hair loss positivity website built in a team using AGILE methodology. Timeline was 2 weeks.",
+    desc: "Pinterest style, hair loss positivity website built in a team using AGILE methodology. Timeline was 2 weeks.",
     languages: "Languages: Javascript",
     tech: "Tech Stack: React, Express, Node.js, MongoDB",
     testing: "Testing: Jest",
     other: "Other Tech: Cloudinary, Bcrypt",
-    functionality: "Functionality: login, signup, image feed/upload, likes, comments, password security",
+    functionality:
+      "Functionality: login, signup, image feed/upload, likes, comments, password security",
     github: "https://github.com/hjtrhodes/baldbibleHR",
     link: "https://baldbible-s8q0.onrender.com/",
     tags: ["MERN", "MongoDB", "React", "Node", "Express"],
@@ -265,13 +260,13 @@ export const projects = [
     name: "Acebook",
     img: "/img/icons/common/Acebook.jpeg",
     mov: "t2ITyMQkPe0",
-    desc:
-      "Facebook clone built in a team using AGILE methodology. Timeline was 2 weeks.",
+    desc: "Facebook clone built in a team using AGILE methodology. Timeline was 2 weeks.",
     languages: "Languages: Javascript, Typescript",
     tech: "Tech Stack: React, Express, Node.js, MongoDB",
     testing: "Testing: Cypress, Jest",
     other: "Other Tech: TailwindCSS, Multer, PostCSS, Vite, Bcrypt",
-    functionality: "Functionality: login, signup, posts, newsfeed, profile page, likes, comments, search, navbar, image uploads",
+    functionality:
+      "Functionality: login, signup, posts, newsfeed, profile page, likes, comments, search, navbar, image uploads",
     github: "https://github.com/hjtrhodes/Acebook-my-version",
     link: "https://acebook-mo3r.onrender.com/",
     tags: ["MERN", "MongoDB", "React", "Node", "Express"],
@@ -280,13 +275,13 @@ export const projects = [
     name: "HomeStay",
     img: "/img/icons/common/HomeStay.jpeg",
     mov: "pBcMyL0oLbo",
-    desc:
-      "AirBnB clone built in a collaborative team using AGILE methodology. Timeline was 1 week.",
+    desc: "AirBnB clone built in a collaborative team using AGILE methodology. Timeline was 1 week.",
     languages: "Languages: Python, SQL, Javascript",
     tech: "Tech Stack: Flask, PostgreSQL",
     testing: "Testing: Pytest",
     other: "",
-    functionality: "Functionality: login, signup, sessions, user spaces, book a space with calendar, approve/reject a space",
+    functionality:
+      "Functionality: login, signup, sessions, user spaces, book a space with calendar, approve/reject a space",
     github: "https://github.com/hjtrhodes/MakersBNBPersonal",
     link: "",
     tags: ["Python", "Flask", "SQL", "Postgres", "Pytest"],
@@ -295,19 +290,18 @@ export const projects = [
     name: "Rock, Paper, Scissors",
     img: "/img/icons/common/RPS.jpeg",
     mov: "JbLGVsDVAZo",
-    desc:
-      "Rock, Paper, Scissors Game. This is a small personal project I created when first learning React to practise using components, managing and passing state and functions as props, rules matrixes and later, my first deployment.",
+    desc: "Rock, Paper, Scissors Game. This is a small personal project I created when first learning React to practise using components, managing and passing state and functions as props, rules matrixes and later, my first deployment.",
     languages: "Languages: Javascript",
     tech: "Tech Stack: Vite, React",
     testing: "",
     other: "",
-    functionality: "Functionality: game logic, win/loss conditional rendering, username display",
+    functionality:
+      "Functionality: game logic, win/loss conditional rendering, username display",
     github: "https://github.com/hjtrhodes/RockPaperScissorReact",
     link: "",
     tags: ["React", "Props", "State"],
   },
 ];
-
 
 export const feedbacks = [
   {
